546a2af77de57dddc132e263959c402b78da42c3
Blaise Thompson <blaise@untzag.com>
2017-10-17 13:08:53 -0500

    __version__ (resolves #3)

--- attune/__init__.py
+++ attune/__init__.py
@@ -1,8 +1,11 @@
 """Tuning tools."""
-
 # flake8: noqa
 
 
+# --- import -------------------------------------------------------------------------------------
+
+
+from .__version__ import *
 from . import coset
 from . import curve
 from . import spectral_delay_correction
