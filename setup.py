85c26e83d2adde62ab8fe6fa00a252b781ee7536
ksunden <sunden@wisc.edu>
2018-07-18 22:39:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 
 # --- import -------------------------------------------------------------------------------------
@@ -7,38 +7,14 @@
 import os
 from setuptools import setup, find_packages
 
-
-# --- functions ----------------------------------------------------------------------------------
-
-
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
-    return paths
-
-
-# ---- objects -----------------------------------------------------------------------------------
-
+def read(fname):
+    return open(os.path.join(here, fname)).read()
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+extra_files = {"attune":["VERSION"]}
 
-extra_files = []
-extra_files.append(os.path.join(here, 'CONTRIBUTORS'))
-extra_files.append(os.path.join(here, 'LICENSE'))
-extra_files.append(os.path.join(here, 'README.rst'))
-extra_files.append(os.path.join(here, 'requirements.txt'))
-extra_files.append(os.path.join(here, 'VERSION'))
-extra_files.append(os.path.join(here, 'WrightTools', 'client_secrets.json'))
-
-
-with open(os.path.join(here, 'requirements.txt')) as f:
-    required = f.read().splitlines()
-
-
-with open(os.path.join(here, 'VERSION')) as version_file:
+with open(os.path.join(here, "attune", 'VERSION')) as version_file:
     version = version_file.read().strip()
 
 
@@ -47,14 +23,16 @@ with open(os.path.join(here, 'VERSION')) as version_file:
 
 setup(
     name='attune',
-    packages=find_packages(),
-    package_data={'': extra_files},
+    packages=find_packages(exclude=("tests", "tests.*"),
+    package_data=extra_files,
+    python_requires=">=3.5",
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    install_requires=required,
-    extras_require={'docs': ['sphinx-gallery>=0.1.9']},
+    tests_require=['pytest', 'pytest-cov'],
+    install_requires=['WrightTools>=3.0', 'numpy', 'scipy', 'matplotlib'],
+    extras_require={'dev': ['black', 'pre-commit']},
     version=version,
     description='Tools for tuning optical parametric amplifiers and multidimensional spectrometers.',
+    long_description=read("README.rst"),
     author='Blaise Thompson',
     author_email='blaise@untzag.com',
     license='MIT',
@@ -65,8 +43,7 @@ setup(
                  'License :: OSI Approved :: MIT License',
                  'Natural Language :: English',
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.3',
-                 'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
-                 'Topic :: Scientific/Engineering']
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Scientific/Engineering'],
 )
