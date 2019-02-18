acf3c0f3d29add4aa6de1ffde7b6f101bf1fb9cc
Kyle Sunden <sunden@wisc.edu>
2019-02-18 10:07:12 -0600

    Add __here__ to tune_test test

--- tests/workup/tune_test/2018-10-12/vpkzk.py
+++ tests/workup/tune_test/2018-10-12/vpkzk.py
@@ -2,10 +2,13 @@ import tempfile
 import attune
 import WrightTools as wt
 import numpy as np
+import pathlib
+
+__here__ = pathlib.Path(__file__).parent
 
 def test_tune_test():
-    d = wt.open("tunetest.wt5")
-    c = attune.Curve.read("in.curve")
+    d = wt.open(__here__ / "tunetest.wt5")
+    c = attune.Curve.read(__here__ / "in.curve")
     d.transform("w3", "wm_points")
     d.wm_points.units = d.wm.units
     d.print_tree()
@@ -13,7 +16,7 @@ def test_tune_test():
     with tempfile.TemporaryDirectory() as td:
         out_tt = attune.workup.tune_test(d, "signal_mean", c, save_directory=td)
 
-    out = attune.Curve.read("out.curve")
+    out = attune.Curve.read(__here__ / "out.curve")
 
     assert np.allclose(out_tt.setpoints[:], out.setpoints[:])
     for d in out.dependents:
