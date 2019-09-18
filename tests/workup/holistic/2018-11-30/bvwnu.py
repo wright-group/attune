57d8ffb405d5f5de64c7a8d43b0517792b9b47bc
Kyle Sunden <sunden@wisc.edu>
2019-09-18 14:56:01 -0500

    Test against current output curve

--- tests/workup/holistic/2018-11-30/bvwnu.py
+++ tests/workup/holistic/2018-11-30/bvwnu.py
@@ -1,6 +1,5 @@
 import matplotlib
 matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
 
 import tempfile
 import attune
@@ -15,15 +14,18 @@ __here__ = pathlib.Path(__file__).parent
 def test_holistic():
     # collect
     d = wt.open(__here__ / "data.wt5")
+
     curve_paths = [__here__ / "old" / "OPA1 (10743) base - 2018-10-26 40490.crv"]
     old = attune.TopasCurve.read(curve_paths, interaction_string='NON-NON-NON-Sig')
-    curve_paths = [__here__ / "out" / "OPA1 (10743) base - 2018-11-30 34255.crv"]
+
+    curve_paths = [__here__ / "out" / "OPA- 2019-09-18 53345.crv"]
     reference = attune.TopasCurve.read(curve_paths, interaction_string='NON-NON-NON-Sig')
+
     # do calculation
     d.transform("w1_Crystal_1", "w1_Delay_1", "wa")
-    new = attune.workup.holistic(d, "array_signal", ["0", "1"], old, gtol=.1, level=True, autosave=False)
-    plt.show()
+    new = attune.workup.holistic(d, "array_signal", ["0", "1"], old, gtol=.05, level=True, autosave=False, save_directory=__here__/"out")
+
     # check
     for r, n in zip(reference.dependents.values(), new.dependents.values()):
         print(r[:].dtype, n[:].dtype)
-        np.testing.assert_allclose(r[:], n[:], atol=0.1)
+        np.testing.assert_allclose(r[:], n[:], rtol=0.01)
