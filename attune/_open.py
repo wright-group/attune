a53e970de87e62660bfaf9a30dd2db657f40b23c
Kyle Sunden <git@ksunden.space>
2020-10-06 15:02:56 -0500

    Make instantiating from nested dictionaries work natively (#96)
    * Make instantiating from nested dictionaries work natively
    
    * import Union
    
    * Unused imports

--- attune/_open.py
+++ attune/_open.py
@@ -2,11 +2,7 @@ __all__ = ["open"]
 
 import json
 
-from ._arrangement import Arrangement
 from ._instrument import Instrument
-from ._setable import Setable
-from ._transition import Transition
-from ._tune import Tune
 
 open_ = open
 
@@ -34,19 +30,4 @@ def open(path, *, load=False):
         with open_(path, "r") as f:
             d = json.load(f)
 
-    arrangements = {}
-    for arrangement_name in d["arrangements"].keys():
-        tunes = {k: Tune(**v) for k, v in d["arrangements"][arrangement_name]["tunes"].items()}
-        arrangements[arrangement_name] = Arrangement(name=arrangement_name, tunes=tunes)
-
-    setables = {k: Setable(**v) for k, v in d["setables"].items()}
-
-    transition = Transition(**d["transition"])
-
-    return Instrument(
-        name=d["name"],
-        arrangements=arrangements,
-        setables=setables,
-        transition=transition,
-        load=load,
-    )
+    return Instrument(**d, load=load)
