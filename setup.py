50c0adb2e84af6fe3c07996eac0dcd66c686192d
Kyle Sunden <git@ksunden.space>
2020-11-16 00:35:21 -0600

    MAINT: update python version (#105)
    * MAINT: update python version
    
    honestly i mostly did this to watch precommit ci reblacken for me, but this change should be done anyway
    Our deps are 3.6+ anyway...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name="attune",
     packages=find_packages(exclude=("tests", "tests.*")),
     package_data=extra_files,
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=[
         "WrightTools>=3.2.5",
         "numpy",
@@ -55,8 +55,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
     ],
 )
