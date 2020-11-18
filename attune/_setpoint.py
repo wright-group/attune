4d3e4a20394aeb26b0fda57401d9bd7b32ff54f2
Kyle Sunden <git@ksunden.space>
2020-11-17 23:01:29 -0600

    Update setpoint (#116)
    * Update setpoint method
    
    * setpoint test

--- attune/_setpoint.py
+++ attune/_setpoint.py
@@ -1,9 +1,13 @@
-"""Methods for processing OPA 800 tuning data."""
+"""Workup an instrument such that the output is the expected color"""
 
 import numpy as np
 import WrightTools as wt
 
-# from .. import Curve, Dependent, Setpoints
+from ._instrument import Instrument
+from ._arrangement import Arrangement
+from ._setable import Setable
+from ._tune import Tune
+from ._transition import Transition
 from ._plot import plot_setpoint
 from ._common import save
 
@@ -34,27 +38,39 @@ def _setpoint(data, channel_name, tune_points, *, spline=True, **spline_kwargs):
         return spline(tune_points).clip(data.axes[1].min(), data.axes[1].max())
     if np.allclose(data.axes[0].points, tune_points):
         return offsets[::-1].clip(data.axes[1].min(), data.axes[1].max())
+    elif np.allclose(data.axes[0].points, tune_points[::-1]):
+        return offsets[::-1].clip(data.axes[1].min(), data.axes[1].max())[::-1]
     else:
-        raise ValueError("Data points and curve points do not match, and splining disabled")
+        raise ValueError("Data points and instrument points do not match, and splining disabled")
 
 
 def setpoint(
-    data, channel, dependent, curve=None, *, autosave=True, save_directory=None, **spline_kwargs
+    data,
+    channel,
+    arrangement,
+    tune,
+    instrument=None,
+    *,
+    autosave=True,
+    save_directory=None,
+    **spline_kwargs
 ):
-    """Workup a generic setpoint plot for a single dependent.
+    """Workup a generic setpoint plot for a single tune.
 
     Parameters
     ----------
     data : wt.data.Data object
-        should be in (setpoint, dependent)
+        should be in (setpoint, tune)
     channel: wt.data.Channel or int or str
         channel to process
-    dependent: str
-        name of the dependent to modify in the curve
-    curve: attune.Curve, optional
-        curve object to modify (Default None: make a new curve)
+    arrangement: str
+        name of the arrangement to modify
+    tune: str
+        name of the tune to modify in the instrument
+    instrument: attune.Curve, optional
+        instrument object to modify (Default None: make a new instrument)
     autosave: bool, optional
-        toggles saving of curve file and images (Defaults to True)
+        toggles saving of instrument file and images (Defaults to True)
     save_directory: Path-like
         where to save (Defaults to current working directory)
     **spline_kwargs: optional
@@ -63,52 +79,58 @@ def setpoint(
     Returns
     -------
     attune.Curve
-        New curve object.
+        New instrument object.
     """
+    metadata = {
+        "channel": channel,
+        "arrangement": arrangement,
+        "tune": tune,
+        "spline_kwargs": spline_kwargs,
+    }
+    if not isinstance(channel, (int, str)):
+        metadata["channel"] = channel.natural_name
+    transition = Transition("setpoint", instrument, metadata=metadata, data=data)
     data = data.copy()
-    # data.convert("wn")
-    if curve is not None:
-        old_curve = curve.copy()
-        # old_curve.convert("wn")
-        setpoints = old_curve.setpoints
+    data.convert("nm")
+    if instrument is not None:
+        old_instrument = instrument.as_dict()
+        setpoints = instrument[arrangement][tune].independent
     else:
-        old_curve = None
-        setpoints = Setpoints(data.axes[0].points, data.axes[0].expression, data.axes[0].units)
+        setpoints = data.axes[0].points
     # TODO: units
+    setpoints.sort()
 
     if isinstance(channel, (int, str)):
         channel = data.channels[wt.kit.get_index(data.channel_names, channel)]
 
     dims = [1] * data.ndim
-    dims[0] = setpoints[:].size  # TODO: be more robust, don't assume 0 index
-    channel -= setpoints[:].reshape(dims)
+    dims[0] = setpoints.size  # TODO: be more robust, don't assume 0 index
+    channel -= setpoints.reshape(dims)
 
-    offsets = _setpoint(data, channel.natural_name, setpoints[:], **spline_kwargs)
+    offsets = _setpoint(data, channel.natural_name, setpoints, **spline_kwargs)
     try:
-        raw_offsets = _setpoint(data, channel.natural_name, setpoints[:], spline=False)
+        raw_offsets = _setpoint(data, channel.natural_name, setpoints, spline=False)
     except ValueError:
         raw_offsets = None
 
-    units = data.axes[1].units
-    if units == "None":
-        units = None
+    if instrument is not None:
+        old_instrument["arrangements"][arrangement]["tunes"][tune]["independent"] = setpoints
+        old_instrument["arrangements"][arrangement]["tunes"][tune]["dependent"] += offsets
+        try:
+            del old_instrument["transition"]
+        except KeyError:
+            pass
+        new_instrument = Instrument(**old_instrument, transition=transition)
+    else:
+        arr = Arrangement(arrangement, {tune: Tune(setpoints, offsets)})
+        new_instrument = Instrument(
+            {arrangement: arr}, {tune: Setable(tune)}, transition=transition
+        )
 
-    new_curve = Curve(
-        setpoints,
-        [Dependent(offsets, dependent, units, differential=True)],
-        name="setpoint",
+    fig, _ = plot_setpoint(
+        data, channel.natural_name, arrangement, tune, new_instrument, instrument, raw_offsets
     )
 
-    if curve is not None:
-        curve = old_curve + new_curve
-    else:
-        curve = new_curve
-
-    # Why did we have to map setpoints?
-    curve.map_setpoints(setpoints[:])
-
-    fig, _ = plot_setpoint(data, channel.natural_name, dependent, curve, old_curve, raw_offsets)
-
     if autosave:
-        save(curve, fig, "setpoint", save_directory)
-    return curve
+        save(new_instrument, fig, "setpoint", save_directory)
+    return new_instrument
