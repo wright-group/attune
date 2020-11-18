16fce600ed472ef3900b400bd9d5c6f13e7db5c3
Kyle Sunden <git@ksunden.space>
2020-11-17 18:36:09 -0600

    Inital rewrite of tune test

--- attune/_tune_test.py
+++ attune/_tune_test.py
@@ -5,8 +5,14 @@ import numpy as np
 
 import WrightTools as wt
 
+from ._instrument import Instrument
+from ._arrangement import Arrangement
+from ._setable import Setable
+from ._tune import Tune
+from ._transition import Transition
 from ._plot import plot_tune_test
 from ._common import save
+from ._map import map_ind_points
 
 __all__ = ["tune_test"]
 
@@ -16,10 +22,11 @@ def _offsets(data, channel_name, tune_points, *, spline=True, **spline_kwargs):
     offsets = data[f"{channel_name}_1_moment_1"].points
 
     if spline:
-        spline = wt.kit.Spline(data.axes[0].points, offsets, **spline_kwargs)
-        return spline(tune_points).clip(data.axes[1].min(), data.axes[1].max())
+        return wt.kit.Spline(data.axes[0].points, offsets, **spline_kwargs)
     if np.allclose(data.axes[0].points, tune_points):
         return offsets.clip(data.axes[1].min(), data.axes[1].max())
+    if np.allclose(data.axes[0].points, tune_points[::-1]):
+        return offsets.clip(data.axes[1].min(), data.axes[1].max())[::-1]
     else:
         raise ValueError("Data points and curve points do not match, and splining disabled")
 
@@ -27,11 +34,13 @@ def _offsets(data, channel_name, tune_points, *, spline=True, **spline_kwargs):
 def tune_test(
     data,
     channel,
-    curve=None,
+    arrangement,
+    curve,
     *,
     level=False,
     gtol=0.01,
     ltol=0.1,
+    restore_setpoints=True,
     autosave=True,
     save_directory=None,
     **spline_kwargs,
@@ -44,14 +53,18 @@ def tune_test(
         should be in (setpoint, dependent)
     channel: wt.data.Channel or int or str
         channel to process
-    curve: attune.Curve, optional
-        curve object to modify (Default None: make a new curve)
+    arrangement: str
+        name of the arrangment to modify
+    curve: attune.Curve
+        curve object to modify
     level: bool, optional
         toggle leveling data (Defalts to False)
     gtol: float, optional
         global tolerance for rejecting noise level relative to global maximum
     ltol: float, optional
         local tolerance for rejecting data relative to slice maximum
+    restore_setpoints: bool, optional
+        toggles remapping onto original setpoints for each tune (default is True)
     autosave: bool, optional
         toggles saving of curve file and images (Defaults to True)
     save_directory: Path-like
@@ -64,16 +77,24 @@ def tune_test(
     attune.Curve
         New curve object.
     """
+    metadata = {
+        "channel": channel,
+        "arrangement": arrangement,
+        "level": level,
+        "gtol": gtol,
+        "ltol": ltol,
+        "spline_kwargs": spline_kwargs,
+    }
+    if not isinstance(channel, (int, str)):
+        metadata["channel"] = channel.natural_name
+    transition = Transition("tune_test", curve, metadata=metadata, data=data)
+        
     data = data.copy()
-    data.convert("wn")
+    data.convert("nm")
     # make data object
-    if curve is not None:
-        old_curve = curve.copy()
-        old_curve.convert("wn")
-        setpoints = old_curve.setpoints
-    else:
-        old_curve = None
-        setpoints = Setpoints(data.axes[0].points, data.axes[0].expression, data.axes[0].units)
+
+    setpoints = data.axes[0].points
+    setpoints.sort()
 
     if isinstance(channel, (int, str)):
         channel = data.channels[wt.kit.get_index(data.channel_names, channel)]
@@ -92,27 +113,31 @@ def tune_test(
     cutoff = np.nanmax(channel[:], axis=1, keepdims=True) * ltol
     channel.clip(min=cutoff)
 
-    offsets = _offsets(data, channel.natural_name, setpoints[:], **spline_kwargs)
+    offset_spline = _offsets(data, channel.natural_name, setpoints, **spline_kwargs)
     try:
-        raw_offsets = _offsets(data, channel.natural_name, data.axes[0].points, spline=False)
+        raw_offsets = _offsets(data, channel.natural_name, setpoints, spline=False)
     except ValueError:
         raw_offsets = None
 
     # make curve ----------------------------------------------------------------------------------
-    new_curve = old_curve.copy()
-    new_curve.setpoints.positions += offsets
-    new_curve.interpolate()
-    new_curve.map_setpoints(setpoints[:], units=setpoints.units)
-    new_curve.convert(curve.setpoints.units)
-    data.axes[0].convert(curve.setpoints.units)
+    old_curve = curve.as_dict()
+    for tune in old_curve["arrangements"][arrangement]["tunes"].values():
+        print(tune)
+        tune["independent"] += offset_spline(tune["independent"])
+    new_curve = Instrument(**old_curve)
+
+    if restore_setpoints:
+        for tune in new_curve[arrangement].keys():
+            new_curve = map_ind_points(new_curve, arrangement, tune, curve[arrangement][tune].independent)
+
+    new_curve._transition = transition
 
     # plot ----------------------------------------------------------------------------------------
 
     fig, _ = plot_tune_test(
         data,
         channel.natural_name,
-        new_curve,
-        used_offsets=offsets,
+        used_offsets=offset_spline(setpoints),
         raw_offsets=raw_offsets,
     )
 
