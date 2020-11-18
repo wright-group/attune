d7c6bd308d9c0028236b534c1c3e8f4b3702d0b1
Kyle Sunden <git@ksunden.space>
2020-11-17 21:53:11 -0600

    use consistent language 'instrument' rather than 'curve'

--- attune/_common.py
+++ attune/_common.py
@@ -3,12 +3,12 @@ import pathlib
 import WrightTools as wt
 
 
-def save(curve, fig, image_name, save_directory=None):
+def save(instrument, fig, image_name, save_directory=None):
     if save_directory is None:
         save_directory = "."
     save_directory = pathlib.Path(save_directory)
     with open(save_directory / "instrument.json", "w") as f:
-        curve.save(f)
+        instrument.save(f)
     # Should we timestamp the image?
     p = (save_directory / image_name).with_suffix(".png")
     wt.artists.savefig(p, fig=fig)
