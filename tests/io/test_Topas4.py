fdf9425857c8487b53d25ddff00275180af226f6
kameyer226 <kentmeyer@wisc.edu>
2021-03-29 13:03:57 -0500

    repeat 2 fixed (maybe) path problem

--- tests/io/test_Topas4.py
+++ tests/io/test_Topas4.py
@@ -6,17 +6,12 @@ from pathlib import Path
 
 script_dir=Path(__file__).parent
 
-def test_fromTopas4():
-<<<<<<< HEAD
+def test_from_topas4():
     testobj = attune.from_topas4(script_dir)
     
-=======
-    testobj = attune.from_topas4("c:/Users/kamey/work/python/attune/tests/io")
-
->>>>>>> 2258fc071b9eb1a7e808c4b49a90968a49d97407
     assert isinstance(testobj, attune.Instrument)
     return
 
 
 if __name__ == "__main__":
-    test_fromTopas4()
+    test_from_topas4()
