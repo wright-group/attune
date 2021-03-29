0e3ea432e99e0147f322a97b749cb7e28db9aa6b
kameyer226 <kentmeyer@wisc.edu>
2021-03-29 12:52:17 -0500

    fixed (maybe) path problem

--- tests/io/test_Topas4.py
+++ tests/io/test_Topas4.py
@@ -2,10 +2,12 @@ import math
 import attune
 import pytest
 import json
+from pathlib import Path
 
+script_dir=Path(__file__).parent
 
 def test_fromTopas4():
-    testobj = attune.from_topas4("c:/Users/kamey/work/python/attune/tests/io")
+    testobj = attune.from_topas4(script_dir)
     
     assert isinstance(testobj, attune.Instrument)
     return
