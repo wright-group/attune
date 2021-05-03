970c3cd2c740f8441607e2041b8237f2f7ec1c81
Kent Meyer <kentmeyer@wisc.edu>
2021-05-03 12:24:14 -0500

    plus an extra assertion on the length of the mins and maxes

--- tests/io/test_Topas4.py
+++ tests/io/test_Topas4.py
@@ -11,10 +11,12 @@ def test_from_topas4():
     testobj = attune.from_topas4(script_dir)
 
     TOTAL_ARRANGEMENTS=3
+    TOTAL_MINS_MAXES=7
     MIN_DEPS_REF=[2.230625, -2.2662695266462407, 2.44375, 0.29442646838236897, 2500.0, 1640.7, 145.44583333333333]
     MIN_INDEPS_REF=[1300.0, 1300.0, 1300.0, 1300.0, 1757.493188010899, 4000.0, 4000.0]
     MAX_DEPS_REF=[4.9759375, 8.030085499389287, 2.65625, -26.225919814858464, 1300.0, 1952.081, 124.17083333333333]
     MAX_INDEPS_REF=[2500.0, 2500.0, 2500.0, 2500.0, 5005.9701492537315, 18000.0, 18000.0]
+    
 
     assert isinstance(testobj, attune.Instrument)
 
@@ -36,6 +38,11 @@ def test_from_topas4():
             min_indeps.append(tune["independent"][0])
             max_deps.append(tune["dependent"][lentune-1])
             max_indeps.append(tune["independent"][lentune-1])            
+
+    assert len(min_deps)==TOTAL_MINS_MAXES
+    assert len(min_indeps)==TOTAL_MINS_MAXES
+    assert len(max_deps)==TOTAL_MINS_MAXES
+    assert len(max_indeps)==TOTAL_MINS_MAXES
 
     for i in range(len(min_deps)):
         assert(min_deps[i]==MIN_DEPS_REF[i])
