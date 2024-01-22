4e1b08ccf3c159cebcb9ff4b5cc08d3ca69b2b77
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2024-01-22 16:54:48 -0600

    cli history
    refine fmt

--- attune/__main__.py
+++ attune/__main__.py
@@ -26,19 +26,21 @@ def catalog():
 
 @main.command(name="history")
 @click.argument("instrument", nargs=1)
-@click.option("-n", default=5)
-def history(instrument, n=5):
+@click.option("-n", default=10)
+def history(instrument, n=10):
     title_string = f"{instrument}, from latest to earliest"
     print(title_string + "-"*(80-len(instrument)))
     current = _store.load(instrument)
     for i in range(n):
         try:            
-            print("{0}{1} at {2}".format(
+            print("{0:4} {1}{2} at {3}".format(
+                -i,
                 current.transition.type,
                 "." * (20-len(current.transition.type)),
                 str(current.load)))
             current = _store.undo(current)
         except ValueError:  # reached end of history
+            print("<end of history>")
             break
 
 
