86da6ed49cf7a8d8d73a4239ef65e70eda0891cf
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-01-16 09:34:08 -0600

    tests

--- tests/discrete_tune.py
+++ tests/discrete_tune.py
@@ -1,12 +1,11 @@
 import attune
+import numpy as np
 
 
 def test_discrete():
     dt = attune.DiscreteTune({"hi": (100, 200), "lo": (10, 20), "med": (20, 100)}, default="def")
-    assert dt(150) == "hi"
-    assert dt(20) == "lo"
-    assert dt(15) == "lo"
-    assert dt(100) == "hi"
-    assert dt(70) == "med"
-    assert dt(5) == "def"
-    assert dt(500) == "def"
+    x = [150, 20, 15, 100, 70, 5, 500]
+    y = ["hi", "lo", "lo", "hi", "med", "def", "def"]
+    assert dt(x) == np.asarray(y)
+    for xi, yi in zip(x,y):
+        assert dt(xi).item() == yi
